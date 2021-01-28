7d9fc5f930e3b4479c940d5dc03b7abd00c8aa5d
intankirana19 <intankirana19@gmail.com>
2021-01-28 16:15:36 +0700

    generate new class based components with rcc enter

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,8 @@
+// type 'rcc' then enter to generate class based components with export on the bottom
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+  render() {
+    return <div></div>;
+  }
+}
